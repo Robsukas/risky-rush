11d9e0bf1bd14d796bce88b888c6eea14e35dec3
heohak <heohak@ttu.ee>
2023-11-11 12:32:49 +0200

    timer

--- app.js
+++ app.js
@@ -5,23 +5,42 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path
-const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
+// Create a container
+const timerContainer = new PIXI.Container();
+timerContainer.x = app.screen.width / 2 + 600;
+timerContainer.y = app.screen.height / 2 - 250;
+app.stage.addChild(timerContainer);
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+// Create a border for the container
+const border = new PIXI.Graphics();
+border.lineStyle(2, 0xFF0000, 1); // Width, color, and alpha of the border
+border.drawRect(-50, -25, 100, 50); // x, y, width, height of the border
+timerContainer.addChild(border);
 
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
 
-app.stage.addChild(bunny);
+// Create a text object for the timer
+const timerText = new PIXI.Text('10.000', { fontFamily: 'Arial', fontSize: 24, fill: 0xffffff, align: 'center' });
+timerText.anchor.set(0.5);
+timerContainer.addChild(timerText);
 
-// Listen for animate update
-app.ticker.add((delta) =>
-{
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+function startCountdown(duration, textElement) {
+    var startTime = Date.now();
+    var countdownInterval = setInterval(function () {
+        var elapsedTime = Date.now() - startTime;
+        var remainingTime = Math.max(duration - elapsedTime, 0);
+        var seconds = Math.floor(remainingTime / 1000);
+        var milliseconds = Math.floor((remainingTime % 1000) / 10); // Dividing by 10 to get centiseconds
+
+        textElement.text = seconds + '.' + (milliseconds < 10 ? '0' : '') + milliseconds;
+
+        if (remainingTime <= 0) {
+            clearInterval(countdownInterval);
+            textElement.text = '0.000';
+            // Actions when countdown ends
+        }
+    }, 10); // Updating every 10 milliseconds
+}
+
+window.onload = function () {
+    startCountdown(10000, timerText); // 10 seconds in milliseconds
+};
