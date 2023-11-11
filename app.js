11361a8f3a4a46696dd1c1bf3073adff4cfdc70c
Robin Nook <ronook@ttu.ee>
2023-11-11 15:44:23 +0200

    movement

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const app = new PIXI.Application({
-    background: '#1099bb',
+    background: '#ede6d5',
     resizeTo: window,
 });
 
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path
-const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
+const circle = new PIXI.Graphics();
+circle.beginFill(0x66CCFF);
+circle.drawCircle(0, 0, 10);
+circle.x = 50;
+circle.y = app.view.height / 2;
+app.stage.addChild(circle);
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+function moveCircle(){
+    const tween = gsap.to(circle, {x: app.screen.width, duration: 10, ease: "none", 
+        onComplete: function () {
+            circle.x = 0;
+            moveCircle();
+        }
+    });
 
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+    setInterval(() => {
+        const newY = Math.random() * app.screen.height;
+        gsap.to(circle, {y: newY, duration: 0.4, ease: "none"});
+    }, 300); // Change y every 1 second here
+}
 
-app.stage.addChild(bunny);
-
-// Listen for animate update
-app.ticker.add((delta) =>
-{
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+moveCircle();
